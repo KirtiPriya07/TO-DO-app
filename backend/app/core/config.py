1a5e966ff01d136c7557dfd123bb5c372e99e802
KirtiPriya07 <kirtipriya64@gmail.com>
2024-04-21 00:49:29 +0530

    no print()

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", cast=str)
     JWT_REFRESH_SECRET_KEY: str = config("JWT_REFRESH_SECRET_KEY", cast=str)
-    print(JWT_REFRESH_SECRET_KEY, JWT_SECRET_KEY, 'bakacatto')
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7   # 7 days
